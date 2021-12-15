37ca03d14371e0c4aa2cd8d08b50ecf4e92c113d
zcl <820861340@qq.com>
2021-12-15 17:30:20 +0800

    junit测试从硬盘上读取图片

--- src/test/ImageTest.java
+++ src/test/ImageTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -16,7 +15,7 @@ import java.io.IOException;
 public class ImageTest {
     @Test
     public void test() throws IOException {
-        BufferedImage bufferedImage = ImageIO.read(new File("F:/tank/src/images/0.gif"));
+        BufferedImage bufferedImage = ImageIO.read(this.getClass().getClassLoader().getResourceAsStream("images/0.gif"));
         Assert.assertNotNull(bufferedImage);
     }
 }
